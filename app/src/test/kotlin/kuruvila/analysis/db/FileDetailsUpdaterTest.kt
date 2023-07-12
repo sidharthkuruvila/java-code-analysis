a8516a00c80ab7a2496b980bd478f03631356320
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2023-07-12 16:40:37 +1000

    Add missed tests

--- app/src/test/kotlin/kuruvila/analysis/db/FileDetailsUpdaterTest.kt
+++ app/src/test/kotlin/kuruvila/analysis/db/FileDetailsUpdaterTest.kt
@@ -1,5 +1,58 @@
 package kuruvila.analysis.db
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.sql.DriverManager
+import kotlin.io.path.Path
+import kotlin.io.path.deleteIfExists
+import kotlin.io.path.writeText
 
-class FileDetailsUpdaterTest
+class FileDetailsUpdaterTest {
+
+
+    var update: FileDetailsUpdater? = null
+    @BeforeEach
+    fun beforEach() {
+        val conn = DriverManager.getConnection("jdbc:sqlite::memory:")
+        update = FileDetailsUpdater(conn)
+        update!!.initialize()
+    }
+    @Test
+    fun testAddingAFile() {
+
+        val checkResult1 = update!!.checkFile(Path("/a/non/existant/File.java"))
+        assertEquals(CheckResult.MISSING, checkResult1)
+
+        val tempFile = Files.createTempFile("test", ".java")
+        tempFile.writeText("Some text")
+
+        val checkResult2 = update!!.checkFile(tempFile)
+        assertEquals(CheckResult.NEW, checkResult2)
+
+        update!!.addFile(tempFile, tempFile)
+
+        val checkResult3 = update!!.checkFile(tempFile)
+        assertEquals(CheckResult.SAME, checkResult3)
+
+        tempFile.writeText("Some other text")
+        val checkResult4 = update!!.checkFile(tempFile)
+        assertEquals(CheckResult.CHANGED, checkResult4)
+
+        update!!.updateFile(tempFile)
+        val checkResult5 = update!!.checkFile(tempFile)
+        assertEquals(CheckResult.SAME, checkResult5)
+
+        tempFile.deleteIfExists()
+        val checkResult6 = update!!.checkFile(tempFile)
+
+        assertEquals(CheckResult.DELETED, checkResult6)
+
+        update!!.removeFile(tempFile)
+
+        val checkResult7 = update!!.checkFile(tempFile)
+        assertEquals(CheckResult.MISSING, checkResult7)
+
+    }
+}
--- app/src/test/kotlin/kuruvila/analysis/db/JavaAstUpdaterTest.kt
+++ app/src/test/kotlin/kuruvila/analysis/db/JavaAstUpdaterTest.kt
@@ -1,5 +1,50 @@
 package kuruvila.analysis.db
 
-import org.junit.jupiter.api.Assertions.*
+import com.github.javaparser.JavaParser
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.sql.Connection
+import java.sql.DriverManager
+import kotlin.io.path.writeText
 
-class JavaAstUpdaterTest
+class JavaAstUpdaterTest {
+    @Test
+    fun testCreatingAnAst() {
+        DriverManager.getConnection("jdbc:sqlite::memory:").use {conn ->
+            val jp = JavaParser()
+            val updater = JavaAstUpdater(conn, jp)
+            updater.initialize()
+            val tempFile = Files.createTempFile("test", ".java")
+            tempFile.writeText("""
+            package kuruvila;
+            public class Klass {
+                int a = 5;
+                Map<String, Long> map = new HashMap<>();
+                String f() {
+                    if(true) {
+                    System.out.println("hello");
+                    }
+                    return "world";
+                }
+            }
+        """.trimIndent())
+            updater.updateFileAst(1, tempFile)
+            assertEquals(41, listAllNodes(conn, 1))
+            updater.deleteAstForFileId(1)
+            assertEquals(0, listAllNodes(conn, 1))
+        }
+    }
+    fun listAllNodes(conn: Connection, fileId: Int): Int {
+        val query = """
+            select count(*) from ast_node where source_file_id = ?
+        """.trimIndent()
+        conn.prepareStatement(query).use { stmt ->
+            stmt.setInt(1, fileId)
+            val rs = stmt.executeQuery()
+            assertTrue(rs.next())
+            return rs.getInt(1)
+        }
+    }
+}
